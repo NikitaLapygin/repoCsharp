901dfee5be0758476c96de59f1b398283b9801de
NIKITA PETROVICH <neketozar@gmail.com>
2023-07-24 20:58:55 +0300

    added Daria example

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,14 @@
+﻿// customized greeting
+Console.Write("Write your name:");
+string? username = Console.ReadLine();
+    if (username.ToLower() == "Daria" || username == "Darya" || username == "Dasha")
+    {
+        Console.WriteLine();
+        Console.WriteLine("Finally! DASHEN'KA JOINED US!");
+        Console.WriteLine();
+    }
+    else 
+    {
+        Console.WriteLine ("Greetings, ");
+        Console.WriteLine(username);
+    }
